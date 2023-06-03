915ebcf3b129836e5ec1a77e79f208c2da416cc3
ash97531 <ashwani.97531@gmail.com>
2023-06-03 18:51:54 +0530

    changes

--- index.server.js
+++ index.server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-const connectDB = require("./db/connect");
+const connectDB = require("./src/db/connect");
 const app = express();
 var cors = require("cors");
-const authRouter = require("./routes/auth");
+const authRouter = require("./src/routes/auth");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
